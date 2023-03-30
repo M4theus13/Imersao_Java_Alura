9c59c48cb4ce94f7050c21683749b668880964c6
Matheus Garcia <matheusgarciajbs@gmail.com>
2023-03-29 21:38:44 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,43 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        // fazer uma conexão HTTP e buscar os tops filmes
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        URI endereço = URI.create(url);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(endereço).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
+        
+        // extrair dados (titulo, poster, classificação)
+<<<<<<< HEAD
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+=======
+        JsonParser parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        System.out.println(listaDeFilmes.size());
+>>>>>>> 0bf28986cf9b7af825feb8f79713275cbc1de89c
+
+        // exibir e manipular dados
+        for (Map<String,String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+<<<<<<< HEAD
+            System.out.println();
+            
+=======
+>>>>>>> 0bf28986cf9b7af825feb8f79713275cbc1de89c
+        }
+    }
+}
